ea1af82953bfdb227e49b377298a96cc6c2161bc
Kuldeep Saini <kuldeep@askmetechnologies.com>
2024-11-29 16:09:36 +0530

    Initial commit

---
+++ config/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'kuldeep';
+$g_db_password            = 'password';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'TLSIo8jijouy04f0OeT2wanGjCTBcPJIZbNfeimoaEU=';
+
+$g_path                   = 'http://bugtracker.local/';
